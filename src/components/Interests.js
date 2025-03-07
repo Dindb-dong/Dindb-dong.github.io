14c4528ac9f2ddeda4ae2cb38f3fa14a6064e179
Dindb-dong <dongwook443@yonsei.ac.kr>
2025-03-07 12:12:02 +0900

    add: Interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,13 +4,15 @@ import '../Interests.css';
 
 const Interests = () => (
   <section className="interests">
-    <h2>관심 분야 & 학습 중인 내용</h2>
-    <ul>
-      <li>인공지능</li>
-      <li>컴퓨터</li>
-      <li>데이터사이언스</li>
-      <li>양자정보학</li>
-    </ul>
+    <div className='wrapper'>
+      <h2>Interestes in & Studying</h2>
+      <ul>
+        <li>AI</li>
+        <li>Computor Science</li>
+        <li>Data Science</li>
+        <li>Quantum information</li>
+      </ul>
+    </div>
   </section>
 );
 
